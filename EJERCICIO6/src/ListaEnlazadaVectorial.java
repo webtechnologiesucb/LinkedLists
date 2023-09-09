f5aed5570e934125e21ea38a1cae25eb35bea003
webtechnologiesucb <miguel.pacheco@ucb.edu.bo>
2023-09-09 13:40:51 -0400

    Ajustes actualizados

--- EJERCICIO6/src/ListaEnlazadaVectorial.java
+++ EJERCICIO6/src/ListaEnlazadaVectorial.java
@@ -18,19 +18,19 @@ public class ListaEnlazadaVectorial {
 
         public void mostrar(){
             Nodo<E> aux;
-            System.out.print("\n CAB->");
+            System.out.print("CAB->");
             for (aux = cab; aux != null; aux = aux.sig)
                 System.out.print(aux.dato + "->");
             System.out.print("NULL\n");
         }
 
         public void agregar_izq(int dato){
-            Nodo<E> nuevo = new Nodo(dato);
+            Nodo<E> nuevo = new Nodo<>(dato);
             nuevo.sig = cab;
             cab = nuevo;
         }
         public void agregar_der(int dato){
-            Nodo<E> nuevo = new Nodo(dato);
+            Nodo<E> nuevo = new Nodo<>(dato);
             Nodo<E> aux;
             if(cab == null)
                 cab = nuevo;
@@ -48,10 +48,48 @@ public class ListaEnlazadaVectorial {
                 aux.dato = aux.dato * valor;
             }
         }
+
+         // Método para sumar dos listas enlazadas
+         public static LinkedList sumarListas(LinkedList lista1, LinkedList lista2) {
+             LinkedList<Integer> resultado = new LinkedList<>();
+             Nodo nodo1 = lista1.cab;
+             Nodo nodo2 = lista2.cab;
+
+             for(nodo1 = lista1.cab, nodo2 = lista2.cab; nodo1 != null && nodo2 != null;
+                 nodo1 = nodo1.sig, nodo2 = nodo2.sig){
+                 resultado.agregar_der(nodo1.dato + nodo2.dato);
+             }
+             // Si las listas son de diferente longitud, agrega los elementos restantes
+             while (nodo1 != null) {
+                 resultado.agregar_der(nodo1.dato);
+                 nodo1 = nodo1.sig;
+             }
+
+             while (nodo2 != null) {
+                 resultado.agregar_der(nodo2.dato);
+                 nodo2 = nodo2.sig;
+             }
+
+             return resultado;
+         }
+
+         // Método para realizar la multiplicación de vectores
+         public static int multiplicarVectores(LinkedList lista1, LinkedList lista2) {
+             int resultado = 0;
+             Nodo nodo1 = lista1.cab;
+             Nodo nodo2 = lista2.cab;
+
+             for(nodo1 = lista1.cab, nodo2 = lista2.cab; 
+                 nodo1 != null && nodo2 != null;
+                 nodo1 = nodo1.sig, nodo2 = nodo2.sig)
+                 resultado += nodo1.dato * nodo2.dato;
+
+             return resultado;
+         }
     }
 
     public static void main(String[] args) {
-        LinkedList<Integer> lista = new LinkedList();
+        LinkedList<Integer> lista = new LinkedList<>();
         lista.agregar_der(5);
         lista.agregar_der(9);
         lista.agregar_der(14);
@@ -59,5 +97,27 @@ public class ListaEnlazadaVectorial {
         int k = 8;
         lista.multiplicaEscalar(k);
         lista.mostrar();
+
+        LinkedList<Integer> lista1 = new LinkedList<>();
+        lista1.agregar_der(1);
+        lista1.agregar_der(2);
+        lista1.agregar_der(3);
+
+        LinkedList<Integer> lista2 = new LinkedList<>();
+        lista2.agregar_der(4);
+        lista2.agregar_der(5);
+        lista2.agregar_der(6);
+
+        System.out.println("Lista 1:");
+        lista1.mostrar();
+        System.out.println("Lista 2:");
+        lista2.mostrar();
+
+        LinkedList<Integer> resultado = LinkedList.sumarListas(lista1, lista2);
+        System.out.println("Resultado de la suma:");
+        resultado.mostrar();
+        int respuesta = resultado.multiplicarVectores(lista1, lista2);
+        System.out.println("Resultado de la multiplicacion: " + respuesta);
+
     }
 }
